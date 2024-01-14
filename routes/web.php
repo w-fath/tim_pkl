a5c49da42d960597d5936d00f8ba90deebc8ec4f
w-fath <fathorrozi14022003@gmail.com>
2024-01-14 13:40:52 +0700

    Pertama

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AppController;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/',[AppController::class,'index'])->name('app.index');
+Route::get('/shop',[ShopController::class,'index'])->name('shop.index');
+Route::get('/product/{slug}',[ShopController::class, 'productDetails'])->name('shop.product.details');
+
+Auth::routes();
+
+Route::middleware('auth')->group(function(){
+    Route::get('/my-account',[UserController::class,'index'])->name('user.index');
+});
+
+Route::middleware(['auth','auth.admin'])->group(function(){
+    Route::get('/admin',[AdminController::class,'index'])->name(('admin.index'));
+});
