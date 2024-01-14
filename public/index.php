a6d545d96ee8867f2de219bfad6521714ddad365
As_Project <140235321+MohSyauqi@users.noreply.github.com>
2024-01-14 13:51:11 +0700

    saya menambahkan cek

--- public/index.php
+++ public/index.php
@@ -15,6 +15,7 @@ define('LARAVEL_START', microtime(true));
 | instead of starting the framework, which could cause an exception.
 |
 */
+// cek
 
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
